cf656699901ff98d5e73d98c1727c80ae181cf02
polarlights <godhuyang@hotmail.com>
2020-06-09 22:05:20 +0800

    init commit

---
+++ lib/asciidoctor/confluence.rb
@@ -0,0 +1,8 @@
+require "asciidoctor/confluence/version"
+
+module Asciidoctor
+  module Confluence
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/asciidoctor/confluence/version.rb
@@ -0,0 +1,5 @@
+module Asciidoctor
+  module Confluence
+    VERSION = "0.1.0"
+  end
+end
